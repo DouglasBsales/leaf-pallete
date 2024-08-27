c03bc384d0772e2cdacca762f3a89ed827914063
douglas sales <douglas-8545@hotmail.com>
2024-08-26 21:32:27 -0300

    commit teste format document with biome

---
+++ components/ComponentTeste.tsx
@@ -0,0 +1,50 @@
+export const ComponentTeste = () => {
+	const teste123 = "teste1";
+
+	return (
+		<div>
+			<h1>Teste component</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nemo
+				ipsa quae provident in? Nisi quibusdam, voluptates accusamus alias modi
+				saepe asperiores consequatur sint fugit voluptas ducimus officiis optio
+				incidunt?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nemo
+				ipsa quae provident in? Nisi quibusdam, voluptates accusamus alias modi
+				saepe asp eriores consequatur sint fugit voluptas ducimus officiis optio
+				incidunt?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nemo i
+				psa quae provident in? Nisi quibusdam, voluptates accusamus alias modi
+				saepe asperiores consequatur sint fugit voluptas ducimus officiis optio
+				incidunt?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nemo
+				ipsa quae provident in? Nisi quibusdam, voluptates accusamus alias modi
+				saepe asperiores consequatur sint fugit voluptas ducimus officiis optio
+				incidunt?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nemo
+				ipsa quae provident in? Nisi quibusdam, voluptates accusamus alias modi
+				saepe asperiores consequatur sint fugit voluptas ducimus officiis optio
+				incidunt?
+			</p>
+			<div>
+				{teste123}
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+						maxime officia omnis libero qui, eius alias minus aliquid fugiat
+						voluptatibus molestiae, ipsa dolor laboriosam nemo numquam vero modi
+						velit reiciendis.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
