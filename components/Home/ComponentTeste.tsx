bf678472c5c5c99b65a056a2b821206afe8799b3
douglas sales <douglas-8545@hotmail.com>
2024-09-03 15:14:24 -0300

    new teste

--- components/Home/ComponentTeste.tsx
+++ components/Home/ComponentTeste.tsx
@@ -26,12 +26,6 @@ export const ComponentTeste = () => {
 					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
 					reiciendis labore. Unde.
 				</p>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
-				</p>
 			</div>
 			<div>
 				<p>
