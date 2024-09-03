35beeb0e88a81339c5ba70454411175c16dbab87
douglas sales <douglas-8545@hotmail.com>
2024-09-03 14:55:46 -0300

    new teste of biome

--- components/Home/ComponentTeste.tsx
+++ components/Home/ComponentTeste.tsx
@@ -3,66 +3,56 @@ export const ComponentTeste = () => {
 		<div>
 			<div>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 			</div>
 			<div>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
-					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
-					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
-					reiciendis labore. Unde.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel facilis quibusdam libero! Veritatis illo
+					architecto deserunt corporis repellendus iure vero dolores quis, doloremque commodi quas. Ipsam, reiciendis
+					labore. Unde.
 				</p>
 			</div>
 		</div>
