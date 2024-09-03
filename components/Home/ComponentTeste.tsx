53349f4a0e4f5ae72e980e8023a063a03eb98849
douglas sales <douglas-8545@hotmail.com>
2024-09-03 14:44:13 -0300

    coding teste of verify identation

---
+++ components/Home/ComponentTeste.tsx
@@ -0,0 +1,72 @@
+export const ComponentTeste = () => {
+	return (
+		<div>
+			<div>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+			</div>
+
+			<div>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi vel
+					facilis quibusdam libero! Veritatis illo architecto deserunt corporis
+					repellendus iure vero dolores quis, doloremque commodi quas. Ipsam,
+					reiciendis labore. Unde.
+				</p>
+			</div>
+		</div>
+	);
+};
